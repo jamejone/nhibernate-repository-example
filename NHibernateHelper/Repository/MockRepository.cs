a0e88c66f1f689243ca1364df63c108d5773deea
james.russel.jones <james.russel.jones@4272040c-1b44-11df-be53-a71ed45b06ac>
2010-02-16 22:03:19 +0000

    Initial upload.

---
+++ NHibernateHelper/Repository/MockRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NHibernateHelper.Repository
+{
+    public class MockRepository<T> : IRepository<T>
+    {
+        public IDictionary<int, T> dictionary;
+
+        public MockRepository()
+        {
+            dictionary = new Dictionary<int, T>();
+        }
+
+        public T Get(object id)
+        {
+            return dictionary[(int)id];
+        }
+
+        public T Save(T value)
+        {
+            dictionary.Add(value.GetHashCode(), value);
+            return value;
+        }
+
+        public void Update(T value)
+        {
+            dictionary.Remove(value.GetHashCode());
+            dictionary.Add(value.GetHashCode(), value);
+        }
+
+        public void Delete(T value)
+        {
+            dictionary.Remove(value.GetHashCode());
+        }
+
+        public IList<T> GetAll()
+        {
+            return dictionary.Values.ToList<T>();
+        }
+    }
+}
