132fd408dc4356ba437801b9e35c3ea57dfe1f48
james.russel.jones <james.russel.jones@4272040c-1b44-11df-be53-a71ed45b06ac>
2010-02-17 22:19:29 +0000

    XML Comments.

--- NHibernateHelper/Repository/IMonkeyRepository.cs
+++ NHibernateHelper/Repository/IMonkeyRepository.cs
@@ -4,6 +4,11 @@ namespace NHibernateHelper.Repository
 {
     public interface IMonkeyRepository : IRepository<Monkey>
     {
+        /// <summary>
+        /// Extending IRepository is appropriate when you need class-specific
+        /// functionality that IRepository doesn't provide. Therefore, the
+        /// function name should be named to fit the business requirement.
+        /// </summary>
         IList<Monkey> GetPooFlingingMonkeys();
     }
 }
